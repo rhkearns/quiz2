fa2ef1faab81bf758b6854cbe7252ee034d5365b
rhkearns <ryankearns217@gmail.com>
2022-03-07 17:13:43 -0500

    add sw questions

--- src/data/quizData.js
+++ src/data/quizData.js
@@ -87,7 +87,287 @@ const quizData = [
 				multChoice: ['68', '394', '188', '208', '142'],
 			},
 		],
-	},
+  },
+  {
+    catTitle: "Star Wars",
+    catImage: 'swImage',
+    questions: [
+      {
+        question: 'What model is the Millennium Falcon?',
+        correctAns: 'Corellian YT-1300f Light Freighter',
+        multChoice: ["Corellian CR90 Corvette", "Kuat EF76 Nebulon-B Escort Frigate", "Corellian YT-1300f Light Freighter", "ST-70 Assault Ship", "Corellian VCX0-100 Light Freighter"],
+      },
+      {
+        question: "What is the name of the Wookie home planet?",
+        correctAns: 'Kashyyyk',
+        multChoice: ["Endor", "Kashyyyk", "Ilum", "Dathomir", "Crait"],
+      },
+      {
+        question: 'What species is Ahsoka Tano?',
+        correctAns: 'Togruta',
+        multChoice: ["Togruta", "Zabrak", "Twi'lek", "Rodian", "Iktochi"],
+      },
+      {
+        question: 'On which planet did Darth Vader establish his base?',
+        correctAns: "Mustafar",
+        multChoice: ["Crait", "Jedha", "Naboo", "Utapau", "Mustafar"],
+      },
+      {
+        question: 'According to Dexter Jettser in Attack of the Clones, what do the analysis droids at the Jedi temple focus on?',
+        correctAns: 'Symbols',
+        multChoice: ["Smell", "Materials", "Particulate Matter", "Symbols", "Digital Cookies"],
+      },
+      {
+        question: "What cell number is Leah Organa in when Luke and Han rescue her from the Death Star (Hint: It is also Finn's Stormtrooper ID number)?",
+        correctAns: "2187",
+        multChoice: ["7567", "5555", "2187", "2224", "1409"],
+      },
+      {
+        question: "What is Luke Skywalker's callsign at the Battle of Yavin?",
+        correctAns: "Red-5",
+        multChoice: ["Blue-2", "Red-5", "Green-7", "Black-Leader", "Gold-1"],
+      },
+      {
+        question: "Which was not a ship taking part in the Rebel attack over Endor?",
+        correctAns: "K-Wing",
+        multChoice: ["X-Wing", "Y-Wing", "B-Wing", "A-Wing", "K-Wing"],
+      },
+      {
+        question: "What creature do Tusken Raiders ride in the deserts of Tatooine?",
+        correctAns: "Bantha",
+        multChoice: ["Tauntaun", "Bantha", "Acklay", "Grinjer", "Boga"],
+      },
+      {
+        question: "Which Sith Lord created the 'Rule of Two'?",
+        correctAns: "Darth Plagueis",
+        multChoice: ["Darth Bane", "Darth Revan", "Darth Sidious", "Darth Tyranus", "Darth Plagueis"],
+      },
+    ]
+  },
+  {
+    catTitle: "Lord of the Rings",
+    catImage: 'lotrImage',
+    questions: [
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+    ]
+  },
+  {
+    catTitle: "Game of Thrones",
+    catImage: 'swImage',
+    questions: [
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+    ]
+  },
+  {
+    catTitle: "Marvel Cinematic Universe",
+    catImage: 'mcuImage',
+    questions: [
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+    ]
+  },
+  {
+    catTitle: "Dungeons & Dragons",
+    catImage: 'ddImage',
+    questions: [
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+      {
+        question: '',
+        correctAns: '',
+        multChoice: [],
+      },
+    ]
+  },
 ];
 
 export {
