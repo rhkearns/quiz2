1d5ee656d7c4068e5eca0b3b5a9886a0bcb4e7d5
rhkearns <ryankearns217@gmail.com>
2022-03-07 16:23:16 -0500

    add hp questions

---
+++ src/data/quizData.js
@@ -0,0 +1,95 @@
+const hpQuestions = [
+	{
+		catTitle: 'Harry Potter',
+		catImage: 'hpImage',
+		questions: [
+			{
+				question: "What do Hermione's parents do for a living?",
+				correctAns: 'Dentists',
+				multChoice: [
+					'Green Grocers',
+					'Dentists',
+					'Bankers',
+					'Chiropractors',
+					'Drill Manufacturers',
+				],
+			},
+			{
+				question:
+					'Which of these was not a monster that Harry faced in the maze in the Third Task of the Triwizard Tournament?',
+				correctAns: 'Grindylow',
+				multChoice: ['Sphinx', 'Skrewt', 'Boggart', 'Grindylow', 'Spider'],
+			},
+			{
+				question: "What is Percy's Owl's Name?",
+				correctAns: 'Hermes',
+				multChoice: ['Hermes', 'Errol', 'Abraxas', 'Arnold', 'Roonil'],
+			},
+			{
+				question: "What form was Ernie Macmillan's Patronus?",
+				correctAns: 'Boar',
+				multChoice: ['Hare', 'Otter', 'Thestral', 'Rat', 'Boar'],
+			},
+			{
+				question:
+					'Who of the following was never a member of the Order of the Phoenix?',
+				correctAns: 'Horace Slughorn',
+				multChoice: [
+					'Nymphadora Tonks',
+					'Severus Snape',
+					'Horace Slughorn',
+					'Elphias Doge',
+					'Aberforth Dumbledore',
+				],
+			},
+			{
+				question: "What is Dean Thomas' favorite Football (soccer) team?",
+				correctAns: 'West Ham United',
+				multChoice: [
+					'West Ham United',
+					'Wolverhampton Wanderers',
+					'Manchester United',
+					'Everton',
+					'A.F.C. Richmond',
+				],
+			},
+			{
+				question: "How many O.W.L.'s did Hermione receive?",
+				correctAns: '10',
+				multChoice: ['11', '14', '13', '10', '21'],
+			},
+			{
+				question: 'What street does Severus Snape live on?',
+				correctAns: "Spinner's End",
+				multChoice: [
+					'Privet Drive',
+					'Mongolia Crescent',
+					"Spinner's End",
+					'Charring Cross Road',
+					'Grimmauld Place',
+				],
+			},
+			{
+				question:
+					'What model broomstick does Ron receive in Order of the Phoenix?',
+				correctAns: 'Cleansweep Eleven',
+				multChoice: [
+					'Nimbus Two-Thousand',
+					'Cleansweep Eleven',
+					'Comet Two-Sixty',
+					'Shooting Star',
+					'Bluebottle',
+				],
+			},
+			{
+				question: 'How many staircases are there at Hogwarts?',
+				correctAns: '142',
+				multChoice: ['68', '394', '188', '208', '142'],
+			},
+		],
+	},
+];
+
+export {
+  hpQuestions
+}  
