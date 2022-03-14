857014dd84a43fed7fb5781e995f389c2764f3aa
rhkearns <ryankearns217@gmail.com>
2022-03-14 12:18:49 -0400

    add links to category cards

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Home.scss';
 import quizData from '../../data/quizData';
 import CategoryCard from '../../components/categoryCard/CategoryCard';
@@ -7,18 +8,22 @@ const Home = () => {
 	// console.log(quizData);
 
 	return (
-    <>
-      <div className="header">
-        <h1 id="title">Nerd Quiz</h1>
-        <h2 id="message">Test your knowledge of the biggest nerdy franchises</h2>
-      </div>
+		<>
+			<div className="header">
+				<h1 id="title">Nerd Quiz</h1>
+				<h2 id="message">
+					Test your knowledge of the biggest nerdy franchises
+				</h2>
+			</div>
 			<div id="categories">
 				{quizData.map(category => (
-					<CategoryCard
-						title={category.catTitle}
-						image={category.catImage}
-						cardClass={category.catCardClass}
-					/>
+					<Link key={category.catPath} to={`/${category.catPath}`}>
+						<CategoryCard
+							title={category.catTitle}
+							image={category.catImage}
+							cardClass={category.catCardClass}
+						/>
+					</Link>
 				))}
 			</div>
 		</>
