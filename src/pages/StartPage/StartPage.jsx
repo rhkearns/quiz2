6e9bbc414443213fb6dc0f363c06c550bad12ff8
rhkearns <ryankearns217@gmail.com>
2022-03-14 12:45:05 -0400

    finding correct category correctly

--- src/pages/StartPage/StartPage.jsx
+++ src/pages/StartPage/StartPage.jsx
@@ -1,14 +1,18 @@
 import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
+import quizData from '../../data/quizData';
 
 const StartPage = () => {
-  const [category, setCategory] = useState('')
+  const [category, setCategory] = useState({})
   const { id } = useParams()
   console.log(id);
 
   useEffect(() => {
+    const selectedCategory = quizData.find(category => category.id === id)
+    setCategory(selectedCategory)
+  }, [])
 
-  })
+  console.log(category);
 
   return (
     <>
