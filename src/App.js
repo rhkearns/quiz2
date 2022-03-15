268363e80d264351ce6dd2d821263a2d5bb2e1e9
rhkearns <ryankearns217@gmail.com>
2022-03-15 11:42:26 -0400

    add quiz route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import NavBar from './components/nav/NavBar';
 import Home from './pages/home/Home';
 import StartPage from './pages/StartPage/StartPage';
+import QuestionPage from './pages/questionPage/QuestionPage';
 
 const App = () => {
 	return (
--- src/pages/StartPage/StartPage.jsx
+++ src/pages/StartPage/StartPage.jsx
@@ -5,14 +5,13 @@ import './StartPage.scss';
 
 const StartPage = () => {
 	const [category, setCategory] = useState({});
-  const { id } = useParams();
-  const navigate = useNavigate()
+	const { id } = useParams();
+	const navigate = useNavigate();
 	console.log(id);
 
-  const handleStart = () => {
-    
-  }
-
+	const handleStart = () => {
+		navigate(`/${id}/quiz`)
+	};
 
 	useEffect(() => {
 		const selectedCategory = quizData.find(category => category.id === id);
@@ -26,7 +25,9 @@ const StartPage = () => {
 			<div className="header">
 				<h1 id="title">{category.title}</h1>
 				<h2 id="message">Are you ready to begin?</h2>
-				<button id="startBtn" onClick={handleStart()}>Begin Quiz</button>
+				<button id="startBtn" onClick={() => handleStart()}>
+					Begin Quiz
+				</button>
 			</div>
 		</>
 	);
