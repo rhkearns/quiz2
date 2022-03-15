c5e6a97ffbabb69bdf1c6a109db6a2fc8dda7c56
rhkearns <ryankearns217@gmail.com>
2022-03-15 11:54:33 -0400

    quesitons displaying correctly

---
+++ src/components/QuestionBlock/QuestionBlock.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const QuestionBlock = ({question}) => {
+  return (
+    <h1>{question.question}</h1>
+  )
+}
+
+export default QuestionBlock
--- src/pages/questionPage/QuestionPage.jsx
+++ src/pages/questionPage/QuestionPage.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import quizData from '../../data/quizData';
+import QuestionBlock from '../../components/QuestionBlock/QuestionBlock';
 
 const QuestionPage = () => {
-  return (
-    <h1>QuestionPage</h1>
-  )
-}
+	const [category, setCategory] = useState({});
+	const { id } = useParams();
 
-export default QuestionPage
+	useEffect(() => {
+		const selectedCategory = quizData.find(category => category.id === id);
+		setCategory(selectedCategory);
+	}, []);
+
+	return (
+		<>
+			<h1 id="title">{category.title}</h1>
+			<div className="questions">
+				{category.questions?.map(question => (
+					<QuestionBlock question={question}/>
+				))}
+			</div>
+		</>
+	);
+};
+
+export default QuestionPage;
