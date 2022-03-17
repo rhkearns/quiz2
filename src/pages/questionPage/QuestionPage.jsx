459b9f93137a611311df92c30280ef5d8631b586
rhkearns <ryankearns217@gmail.com>
2022-03-17 12:53:25 -0400

    style questionpage

--- src/pages/questionPage/QuestionPage.jsx
+++ src/pages/questionPage/QuestionPage.jsx
@@ -1,37 +1,53 @@
 import React, { useState, useEffect } from 'react';
+import './QuestionPage.scss';
 import { useParams } from 'react-router-dom';
 import quizData from '../../data/quizData';
 import QuestionBlock from '../../components/QuestionBlock/QuestionBlock';
 
 const QuestionPage = () => {
 	const [category, setCategory] = useState({});
-  const { id } = useParams();
-  const [correctAnswers, setCorrectAnswers] = useState([])
-  const [guesses, setGuesses] = useState([]);
+	const { id } = useParams();
+	const [correctAnswers, setCorrectAnswers] = useState([]);
+	// const [guesses, setGuesses] = useState([
+	// 	'',
+	// 	'',
+	// 	'',
+	// 	'',
+	// 	'',
+	// 	'',
+	// 	'',
+	// 	'',
+	// 	'',
+	// 	'',
+	// ]);
 
-  const handleSelectAnswer = (selection, idx) => {
+	// const handleSelectAnswer = (selection, idx) => {
+	// 	setGuesses(...guesses, guesses.splice(idx, 1, selection));
+	// };
 
-  }
+	const handleCheckAnswers = () => {};
 
 	useEffect(() => {
 		const selectedCategory = quizData.find(category => category.id === id);
-    setCategory(selectedCategory);
-    setCorrectAnswers(selectedCategory.questions.map(q => {
-      return q.correctAns
-    }))
+		setCategory(selectedCategory);
+		setCorrectAnswers(
+			selectedCategory.questions.map(q => {
+				return q.correctAns;
+			})
+		);
 	}, []);
 
-console.log(correctAnswers);
-
 	return (
-		<>
+		<div className='question-page'>
 			<h1 id="title">{category.title}</h1>
 			<div className="questions">
 				{category.questions?.map(question => (
-					<QuestionBlock question={question}/>
+					<div className="question-block">
+						<QuestionBlock question={question} />
+					</div>
 				))}
 			</div>
-		</>
+		</div>
 	);
 };
 
