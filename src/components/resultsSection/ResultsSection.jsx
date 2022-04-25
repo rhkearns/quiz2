c63ca5320e1f6d0111567fd5a6ed15b4d00f1af3
rhkearns <ryankearns217@gmail.com>
2022-04-25 14:25:15 -0400

    result section basic style

--- src/components/resultsSection/ResultsSection.jsx
+++ src/components/resultsSection/ResultsSection.jsx
@@ -4,10 +4,10 @@ import './ResultsSection.scss';
 const ResultsSection = ({ score }) => {
 	return (
 		<div id="results">
-			{score <= 5 && <h1>TOO BAD</h1>}
-			{score > 5 && score < 8 && <h1>WELL DONE</h1>}
-			{score >= 8 && <h1>CONGRATULATIONS</h1>}
-			<h3>You answered {score} out of 10 correctly!</h3>
+			{score <= 5 && <h1 id="result-head">TOO BAD</h1>}
+			{score > 5 && score < 8 && <h1 id="result-head">WELL DONE</h1>}
+			{score >= 8 && <h1 id="result-head">CONGRATULATIONS</h1>}
+			<h3 id="result-message">You answered {score} out of 10 correctly!</h3>
 		</div>
 	);
 };
