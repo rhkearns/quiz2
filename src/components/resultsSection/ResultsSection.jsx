8712274f8c29f9a02f97bc2d9915162bf55b491e
rhkearns <ryankearns217@gmail.com>
2022-04-25 15:15:57 -0400

    add return to home link in results

--- src/components/resultsSection/ResultsSection.jsx
+++ src/components/resultsSection/ResultsSection.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './ResultsSection.scss';
 
-const ResultsSection = ({ score }) => {
+const ResultsSection = ({ score }) => {	
 	return (
 		<div id="results">
 			{score <= 5 && <h1 id="result-head">TOO BAD</h1>}
 			{score > 5 && score < 8 && <h1 id="result-head">WELL DONE</h1>}
 			{score >= 8 && <h1 id="result-head">CONGRATULATIONS</h1>}
 			<h3 id="result-message">You answered {score} out of 10 correctly!</h3>
+			<Link id="link-home" to={`/`}>
+				<h3>Return to Home</h3>
+			</Link>
 		</div>
 	);
 };
